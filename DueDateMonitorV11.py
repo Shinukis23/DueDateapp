16c9599e1417de5997d8f6b9e0863e26aee2da16
Shinukis23 <juan.a.o.delgado@gmail.com>
2023-09-02 18:19:22 -0700

    Se agrego funcion para calcular tipo de cambio

--- DueDateMonitorV11.py
+++ DueDateMonitorV11.py
@@ -17,8 +17,10 @@ import pytz
 import pandas as pd
 import numpy as np
 import xlwt
+import ssl
 import openpyxl
 import xlsxwriter
+import locale
 import gspread
 import pygsheets
 from oauth2client.service_account import ServiceAccountCredentials
@@ -35,12 +37,17 @@ import gspread_dataframe as gd
 import gdown
 import io
 import os
+from urllib.request import urlopen
 from urllib.parse import urlparse
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import requests
 from bs4 import BeautifulSoup
+sess = requests.Session()
+adapter = requests.adapters.HTTPAdapter(max_retries = 20)
+sess.mount('http://', adapter)
+
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -53,22 +60,87 @@ scopes = [
 'https://www.googleapis.com/auth/spreadsheets',
 'https://www.googleapis.com/auth/drive'
 ]
+
+proxies = {'https': 'http:tapaserver.dyndns.org'}
 credentials = ServiceAccountCredentials.from_json_keyfile_name("monitor-eficiencia-3a13458926a2.json", scopes) #access the json key you downloaded earlier 
 file = gspread.authorize(credentials)# authenticate the JSON key with gspread
 ss = file.open("EficienciaReporte")
-#########333
-
-#from oauth2client.service_account import ServiceAccountCredentials
-
-# Credenciales
-#credentials = ServiceAccountCredentials.from_json_keyfile_name("monitor-eficiencia-3a13458926a2.json", scopes)  # Reemplaza con tu ruta
-#client = gspread.authorize(credentials)
-
-####
-###
-
 service = build('drive', 'v3', credentials=credentials)
 
+url3 = "https://www.banxico.org.mx/SieInternet/consultarDirectorioInternetAction.do?sector=6&accion=consultarCuadro&idCuadro=CF102&locale=es"
+html = sess.get(url3).content
+df_list = pd.read_html(html)
+df = df_list[0]
+#print(df)
+resultWeekend=df.iloc[8,2]
+
+i = 1
+super_user = 0 
+result = ""
+result1 = ""
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+DOF = url = URL = page = soup = texto = "" 
+cambio = 0
+entry1 = 0
+Tipo_Cambio = 0.0
+
+def scrap_web():
+    global DOF
+    global Tipo_Cambio
+    global result
+    global result1
+    global diff
+    global i
+    url =  service.files().get_media(fileId="18F4Ix9C_2q7tjimDycg6v7XnCAb258FM").execute()
+    URL = url.decode('utf-8')
+    #url = "https://dof.gob.mx/index.php#gsc.tab=0"
+    unverified_context = ssl._create_unverified_context()
+    page = urlopen(URL, context=unverified_context)
+    #page = urlopen(URL)
+    url =  service.files().get_media(fileId="1Y-5sSIKrF1HmV58gPJpTWZE7yB4Qa9Ee").execute()
+    diff = float(url.decode('utf-8'))
+    ##print(diff)
+    page = page.read().decode("utf-8")
+    soup = BeautifulSoup(page, "html.parser")
+    texto =soup.get_text()
+    if datetime.today().weekday() != 6 and datetime.today().weekday() != 5:
+    #if datetime.today().weekday() == 6 or datetime.today().weekday() == 5:
+        resultado = re.search('DOLAR (.*\d)UDIS', texto)
+        #resultado1 = re.search('Tipo de Cambio y Tasas al (.*)', texto)
+        #result1 = resultado1.group(1)
+        #print(resultado)
+        if resultado is None:
+            result = resultWeekend
+            date = datetime.today() - timedelta(days = 3)
+            result1 = date.strftime("%Y/%m/%d")#, %H:%M:%S")
+            #print("test",result1)
+        else:
+            result=resultado.group(1)
+            resultado1 = re.search('Tipo de Cambio y Tasas al (.*)', texto)
+            result1 = resultado1.group(1)
+            #print("este es",result) 
+            
+    elif datetime.today().weekday() == 6 or datetime.today().weekday() == 5:
+    #elif datetime.today().weekday() != 6 and datetime.today().weekday() != 5:
+        result = resultWeekend
+        resultado1 = re.search('Tipo de Cambio y Tasas al (.*)', texto)
+        result1 = resultado1.group(1)
+        date=dt.strptime(result1, "%d/%m/%Y")
+        if datetime.today().weekday() == 6: 
+            date = date - timedelta(days = 2)
+            #print(date)  
+        elif datetime.today().weekday() == 5: 
+            date = date - timedelta(days = 1)
+            
+        result1 = date.strftime("%Y/%m/%d")#, %H:%M:%S")
+        print("Es dia inhabil:",result) 
+    Tipo_Cambio = float(result)
+    DOF = "Tipo de Cambio y Tasas al "+ result1
+    i = i + 1
+    print(datetime.today().strftime("%H:%M:%S"))
+
+
+scrap_web()
 # Define the URL to download the file from
 file_url = service.files().get(fileId=file_id0, fields="webContentLink").execute()["webContentLink"]
 parsed_url = urlparse(file_url)
@@ -166,6 +238,8 @@ df= df.set_index('Store')
 #print(dc)
 dc = dict(dc.set_index('DIA').groupby(level = 0).\
     apply(lambda x : x.to_dict(orient= 'list')))
+
+
 def ExportFile():
     global credentials
 #    global url1
@@ -206,24 +280,37 @@ ExportFile()
 #print("fuera de la funcion:")
 #print(dfpartes.loc[5, 'No Parte'])
 
-def Actualizacion():
-    #global DOF
-    #global resultWeekend
-    #global diff
-    #global result1
-    #global result
-    #global Tipo_Cambio
-    while True:
-        ExportFile()
-        #tk.Label(main_window,font=("Courier 9 bold"),fg="red", text = DOF).place(x = 14,y = 40)
-        time.sleep(5*60*60)
+#aqui va
 
 #Create a separate thread for printing the message
-thread = threading.Thread(target=Actualizacion)
-thread.daemon = True  # Set the thread as a daemon so it won't prevent the script from exiting
-thread.start()
+#thread = threading.Thread(target=Actualizacion)
+#thread.daemon = True  # Set the thread as a daemon so it won't prevent the script from exiting
+#thread.start()
+control = 1
 
 def main_program(super_user,hoja):
+
+    def Actualizacion():
+        global DOF
+        global resultWeekend
+        global diff
+        global result1
+        global result
+        global control
+        global Tipo_Cambio
+        while True:
+            ExportFile()
+            scrap_web()
+            if control == 1:
+                #T_Cambio = tk.Label(main_window,font=("times", 10, "bold"),fg="green",text=Tipo_Cambio).place(x=270,y=16)  
+                T_Cambio = tk.Label(main_window,font=("times", 10, "bold"),fg="green",text=Tipo_Cambio).place(x=263,y=16)
+                control = 0
+            else :    
+                #T_Cambio = tk.Label(main_window,font=("times", 10, "bold"),fg="red",text=Tipo_Cambio).place(x=270,y=16)
+                T_Cambio = tk.Label(main_window,font=("times", 10, "bold"),fg="red",text=Tipo_Cambio).place(x=263,y=16)
+                control = 1#time.sleep(1)
+            #time.sleep(5*60*60)
+            time.sleep(2*60*60)
 
     def add_action():
         
@@ -430,6 +517,10 @@ def main_program(super_user,hoja):
             data.extend(["-","-","-","-","-","-"])   
             data.extend([dates])
             subir()
+
+#########
+
+###            
 
     def clearOrden():
         #data.clear()
@@ -921,6 +1012,10 @@ def main_program(super_user,hoja):
     combobox.set("15 EAST")
     
     #tk.Label(main_window, text='Seleccione la ruta o locacion de entrega: ',font=("Courier 10 bold"),fg="blue").place(x=0,y=230)
+    tk.Label(main_window, text='Ver.11 Ago/2023',font=("Courier 6 bold"),fg="black").place(x=210,y=2)
+    Cambio = tk.Label(main_window,font=("times", 10, "bold"),fg="black",text="Tipo_Cambio: ").place(x=187,y=16)
+    T_Cambio = tk.Label(main_window,font=("times", 10, "bold"),fg="red",text=Tipo_Cambio).place(x=263,y=16)
+    #name = Label(main_window,font=("Courier 9 bold"), text = DOF).place(x = 14,y = 4)
     tk.Label(main_window, text='Ver.11 Ago/2023',font=("Courier 6 bold"),fg="black").place(x=210,y=2)        
     # Add button
     add_button = tk.Button(main_window, text="Add", command=add_action,state=tk.DISABLED)#.place(x=10,y=450)
@@ -956,6 +1051,9 @@ def main_program(super_user,hoja):
     dates = ''
    # compromiso=''
     cambio = 0
+    thread = threading.Thread(target=Actualizacion)
+    thread.daemon = True  # Set the thread as a daemon so it won't prevent the script from exiting
+    thread.start()
     main_window.attributes('-topmost',True)
     main_window.mainloop()
 
@@ -1006,4 +1104,7 @@ def login():
     login_window.mainloop()
     
 selected_user = ""
+#thread = threading.Thread(target=Actualizacion)
+#thread.daemon = True  # Set the thread as a daemon so it won't prevent the script from exiting
+#thread.start()
 login()
